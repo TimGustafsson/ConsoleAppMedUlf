b07b5e9aeb183332d484f970ec82198e9f0af225
Tim Gustafsson <99013177+timgustaf@users.noreply.github.com>
2022-02-14 08:17:53 +0100

    Added a while loop and a menu statement.

--- ConsoleAppMedUlf/Program.cs
+++ ConsoleAppMedUlf/Program.cs
@@ -5,39 +5,62 @@ namespace ConsoleAppMedUlf
     class Program
     {
         static void Main(string[] args)
+        {//Code block of main method
+
+            while (true)
+            {
+
+                PrintMenu();
+
+                Console.WriteLine("Enter a number here");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter a second number here");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                Addition(num1, num2);
+
+                Subtraction(num1, num2);
+
+                Division(num1, num2);
+
+                Multiplication(num1, num2);
+
+            }
+
+        }//End of main method
+
+        //Menu
+        private static void PrintMenu()
         {
-            Console.WriteLine("Enter a number here");
-            double num = Convert.ToDouble(Console.ReadLine());
-
-
-            Console.WriteLine("Enter a second number here");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            Addition(num, num2);
-
-            Subtraction(num, num2);
-
-            Multiplication(num, num2);
-
-            Division(num, num2);
+            Console.WriteLine("-----Menu-----");
+            Console.WriteLine("Result order:");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Divison");
+            Console.WriteLine("4. Multiplication");
         }
 
-        static private void Addition(double num, double num2)
+        // Addition
+        static private void Addition(double num1, double num2)
         {
-            Console.WriteLine($"{num} + {num2} = {num + num2}");
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
         }
 
-        static private void Subtraction(double num, double num2)
+        //Subtraction
+        static private void Subtraction(double num1, double num2)
         {
-            Console.WriteLine($"{num} - {num2} = {num - num2}");
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
         }
 
-        static private void Multiplication(double num, double num2)
+        //Multiplication
+        static private void Multiplication(double num1, double num2)
         {
-            Console.WriteLine($"{num} * {num2} = {num * num2}");
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
         }
 
-        static private void Division(double num, double num2)
+        //Division
+        static private void Division(double num1, double num2)
         {
 
             if (num2 == 0)
@@ -46,15 +69,11 @@ namespace ConsoleAppMedUlf
             }
             else
             {
-                Console.WriteLine($"{num} / {num2} = {num / num2}");
+                Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
             }
                     
-
         }
 
+    }//End of class
 
-
-
-
-    }
-}
+} //End of name
